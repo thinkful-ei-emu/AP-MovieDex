d869ac91c48b99b352716a5398f27b1405159dbe
Arpan Patel <adp8ke@gmail.com>
2019-07-19 17:12:34 -0400

    set up to deploy

--- server.js
+++ server.js
@@ -7,20 +7,33 @@ require("dotenv").config();
 
 const server = express();
 
-server.use(morgan("dev"));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
+
 server.use(helmet());
 server.use(cors());
 
 server.use(function validateBearerToken(req, res, next) {
     const authToken = req.get('Authorization');
     const apiToken = process.env.API_TOKEN;
-    console.log('validate bearer token');
 
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({error: 'Unauthorized request'});
     }
     next();
 });
+
+server.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 function handleGetMovies(req, res) {
   let movies = [...array];
@@ -32,25 +45,16 @@ function handleGetMovies(req, res) {
       return movie.genre.toLowerCase().includes(genre.toLowerCase());
     });
 
-    // if(movies === []){
-    //     return res.status(404).json("Please enter a valid genre")
-    // }
   }
 
   if (country) {
     movies = movies.filter(movie => {
       return movie.country.toLowerCase().includes(country.toLowerCase());
     });
-
-    //  if(!movies){
-    //     return res.status(404).json("Please enter a valid country")
-    // }
   }
 
   if (avg_vote) {
-    // if(Number(avg_vote) === NaN){
-    //     return res.status(404).json("Enter a valid number")
-    // }
+
 
     movies = movies.filter(movie => {
       return Number(movie.avg_vote) >= Number(avg_vote);
@@ -66,7 +70,7 @@ function handleGetMovies(req, res) {
 
 server.get("/movie", handleGetMovies);
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000
 
 server.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
